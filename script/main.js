9ed890acb7166be2616ab55e19ebefa789179f65
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-07-28 19:43:18 -0500

    Created the generateGrid function, which creates the initial skecth box

--- script/main.js
+++ script/main.js
@@ -1,15 +1,28 @@
-const container = document.getElementById("container");
+const container = document.querySelector("#container");
 
-let squares = 16;
 
-container.style.setProperty("grid-template-columns", "repeat(" + squares + ", 1fr)");
-container.style.setProperty("grid-template-rows", "repeat(" + squares + ", 1fr)");
-
-for (let i = 1; i <= squares * squares; i++) {
-    const div = document.createElement("div");
-    div.classList.add("pixels");
-    div.style.border = "1px solid black";
-
-    container.appendChild(div);
-}
+function generateGrid(){
+    let squares = 16;
+    
+    container.style.setProperty("grid-template-columns", "repeat(" + squares + ", 1fr)");
+    container.style.setProperty("grid-template-rows", "repeat(" + squares + ", 1fr)");
+    
+    for (let i = 1; i <= squares * squares; i++) {
+        const div = document.createElement("div");
+        div.classList.add("pixels");
+        div.style.border = "1px solid rgba(0,0,0,0.5)";
+    
+        container.appendChild(div);
+    }
+    
+    const pixels = document.querySelectorAll(".pixels");
+    
+    console.log(pixels);
+    
+    pixels.forEach(function(pixel){
+        pixel.addEventListener("mouseover", () => {
+            pixel.style.backgroundColor = "black";
+        })
+    })
+}    
 
