36ec20c6e2e97c3e9cf1708b6b8e493174100def
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-07-28 20:27:17 -0500

    Added button to generate the grid and a function to clear the content of the grid

--- script/main.js
+++ script/main.js
@@ -1,28 +1,51 @@
 const container = document.querySelector("#container");
 
 
-function generateGrid(){
-    let squares = 16;
-    
+function generateGrid() {
+    let squares;
+
+    while (true) {
+        squares = prompt("Enter a number of squares per side between 0 and 100", "16");
+        // squares = 16;
+        squares = parseInt(squares, 10)
+        if (squares != NaN && squares > 0 && squares <= 100) {
+            break;
+        }
+    }
+
     container.style.setProperty("grid-template-columns", "repeat(" + squares + ", 1fr)");
     container.style.setProperty("grid-template-rows", "repeat(" + squares + ", 1fr)");
-    
-    for (let i = 1; i <= squares * squares; i++) {
+
+    for (let i = 1; i <= +squares * +squares; i++) {
         const div = document.createElement("div");
         div.classList.add("pixels");
         div.style.border = "1px solid rgba(0,0,0,0.5)";
-    
+
         container.appendChild(div);
     }
-    
-    const pixels = document.querySelectorAll(".pixels");
-    
-    console.log(pixels);
-    
-    pixels.forEach(function(pixel){
+
+    window.pixels = document.querySelectorAll(".pixels");
+    // console.log(pixels);
+
+    pixels.forEach(function (pixel) {
         pixel.addEventListener("mouseover", () => {
             pixel.style.backgroundColor = "black";
         })
     })
-}    
+}
+
+function clear(){
+    pixels.forEach(function (pixel){
+        pixel.style.backgroundColor = "white";
+        container.removeChild(pixel);
+    })
+}
+
+generateGrid();
+
+const button = document.querySelector("#generate");
+button.addEventListener("click", () => {
+    clear();
+    generateGrid();
+});
 
