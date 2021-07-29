1c604cbce686c53509c59e5d0f6faca198fb89f8
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-07-28 20:34:32 -0500

    Improved functionality by adding a default loading value

--- script/main.js
+++ script/main.js
@@ -1,32 +1,22 @@
 const container = document.querySelector("#container");
 
 
-function generateGrid() {
-    let squares;
-
-    while (true) {
-        squares = prompt("Enter a number of squares per side between 0 and 100", "16");
-        // squares = 16;
-        squares = parseInt(squares, 10)
-        if (squares != NaN && squares > 0 && squares <= 100) {
-            break;
-        }
-    }
-
+function generateGrid(squares) {
+    
     container.style.setProperty("grid-template-columns", "repeat(" + squares + ", 1fr)");
     container.style.setProperty("grid-template-rows", "repeat(" + squares + ", 1fr)");
-
-    for (let i = 1; i <= +squares * +squares; i++) {
+    
+    for (let i = 1; i <= squares * squares; i++) {
         const div = document.createElement("div");
         div.classList.add("pixels");
         div.style.border = "1px solid rgba(0,0,0,0.5)";
-
+        
         container.appendChild(div);
     }
-
+    
     window.pixels = document.querySelectorAll(".pixels");
     // console.log(pixels);
-
+    
     pixels.forEach(function (pixel) {
         pixel.addEventListener("mouseover", () => {
             pixel.style.backgroundColor = "black";
@@ -41,11 +31,18 @@ function clear(){
     })
 }
 
-generateGrid();
+generateGrid(16);
 
 const button = document.querySelector("#generate");
 button.addEventListener("click", () => {
     clear();
-    generateGrid();
+    while (true) {
+        let squares = prompt("Enter a number of squares per side between 0 and 100", "16");
+        squares = parseInt(squares, 10)
+        if (squares != NaN && squares > 0 && squares <= 100) {
+            generateGrid(squares);
+            break;
+        }
+    }
 });
 
