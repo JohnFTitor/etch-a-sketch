2d6d2a158019e5a5df48117310a6971aee813de6
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-05 17:44:02 -0500

    Added 'Increasing 10% of black' functionality to color drawing

--- script/main.js
+++ script/main.js
@@ -34,14 +34,23 @@ function drawBlack(pixel) {
 }
 
 function drawRGB(pixel){
+    let h = Math.floor(Math.random()*360);
+    let s = Math.floor(Math.random()*100);
+    let l = 90;
     pixel.addEventListener("mouseover", () => {
-        pixel.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+        pixel.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
+        if (pixel.style.backgroundColor != "white") {
+            pixel.addEventListener("mouseover", () => {
+                if(l != 0){
+                    l -= 2;
+                }
+                pixel.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
+            })
+        }
     })    
 }
 
-function randomRGB(){
-    return Math.floor(Math.random() * 250);
-}
+
 
 
 
